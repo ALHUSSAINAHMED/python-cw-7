35e11178f4d245f83272c35b59fbb28218df6197
ALHUSSAINAHMED <hussainalyasiri20@gmail.com>
2023-09-09 19:00:46 +0300

    HUSSAIN7

---
+++ classwork7.py
@@ -0,0 +1,51 @@
+class person:
+    def __init__(self, name, age):
+        self.name = "hussain"
+        self.age = "15"
+
+    def __str__(self):
+        return f"my name is hussain, and iam 15 years old"
+
+    def is_adult(person):
+        if first_person.age > 18:
+            print("You have too much responsibilities")
+
+        elif first_person.age < 18:
+            print("lucky you")
+
+
+first_person = person("name","age")
+print(first_person.name , first_person.age)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
